ae79c229027b3513c55fea187c6c0ec094f9966b
EgeSaykan <shk.ync2005@gmail.com>
2024-11-03 00:41:39 +0000

    Connecting

---
+++ Network/connect.py
@@ -0,0 +1,22 @@
+import network
+from network import Network
+from server import start_server
+from time import sleep
+from _thread import *
+
+collective_data = {
+  "player0" : {},
+  "player1" : {},
+  "player2" : {},
+  "player3" : {}
+}
+
+
+host = "169.254.131.250"
+
+start_new_thread(start_server, (collective_data))
+
+def waiting_screen():
+  for i in collective_data:
+    if not collective_data[i]:
+      return 4 - int(i[-1])
--- Network/server.py
+++ Network/server.py
@@ -3,14 +3,8 @@ from _thread import *
 from json import dumps, loads
 import sys
 
-collective_data = {
-  "player0" : {},
-  "player1" : {},
-  "player2" : {},
-  "player3" : {}
-}
 
-def threaded_client(conn):
+def threaded_client(conn, collective_data, index):
   conn.send(str.encode("True"))
   data = loads(conn.recv(2048 * 16).decode("utf-8"))
   collective_data["player" + str(data["player-id"])] = data
@@ -26,8 +20,7 @@ def threaded_client(conn):
 
   conn.close()
 
-def start_server():
-  global collective_data
+def start_server(collective_data):
   server = socket.gethostbyname(socket.gethostname())
   print(server)
   port = 8000
@@ -46,5 +39,7 @@ def start_server():
   while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
-
-    start_new_thread(threaded_client, (conn,))
+    for i in collective_data:
+      if not collective_data[i]:
+        index = i
+    start_new_thread(threaded_client, (conn, collective_data, index))
